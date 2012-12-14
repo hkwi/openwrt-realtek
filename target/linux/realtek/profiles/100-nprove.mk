b8c7700266d9eee9e49ba91a5be70498d4f22413
Roman Yeryomin <roman@advem.lv>
2012-12-14 22:43:18 +0200

    Add more packages to nprove profile
    Signed-off-by: Roman Yeryomin <roman@advem.lv>

--- target/linux/realtek/profiles/100-nprove.mk
+++ target/linux/realtek/profiles/100-nprove.mk
@@ -1,8 +1,9 @@
 define Profile/nprove
   NAME:=nprove
-  PACKAGES:=kmod-tun kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+  PACKAGES:=kmod-tun kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 kmod-usb-serial kmod-usb-serial-option \
+	kmod-usb-storage kmod-usb-printer \
 	luci luci-app-diag-devinfo luci-app-p910nd luci-app-qos luci-app-wol \
-	librt libiw
+	librt libiw query3g maccalc
 endef
 
 define Profile/nprove/Description
