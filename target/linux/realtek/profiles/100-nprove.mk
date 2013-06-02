963eaf3485d0d532d197a1835ca9fd92101914e5
Roman Yeryomin <romans.jerjomins@saftehnika.com>
2013-06-02 17:10:10 +0300

    repartition default packages
    Signed-off-by: Roman Yeryomin <romans.jerjomins@saftehnika.com>

--- target/linux/realtek/profiles/100-nprove.mk
+++ target/linux/realtek/profiles/100-nprove.mk
@@ -1,9 +1,11 @@
 define Profile/nprove
   NAME:=nprove
-  PACKAGES:=kmod-tun kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 kmod-usb-serial kmod-usb-serial-option \
+  PACKAGES:= \
+	kmod-tun kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+	kmod-usb-serial kmod-usb-serial-option \
 	kmod-usb-storage kmod-usb-printer \
 	luci luci-app-diag-devinfo luci-app-p910nd luci-app-qos luci-app-wol \
-	librt libiw query3g maccalc
+	librt libiw query3g maccalc libopenssl
 endef
 
 define Profile/nprove/Description
