0540aa9b736221755dcdb717bc96b3aedf89ec2e
Roman Yeryomin <roman@advem.lv>
2013-07-07 11:26:55 +0300

    Add firewall package to defaults for nprove profile.
    Signed-off-by: Roman Yeryomin <roman@advem.lv>

--- target/linux/realtek/profiles/100-nprove.mk
+++ target/linux/realtek/profiles/100-nprove.mk
@@ -6,7 +6,8 @@ define Profile/nprove
 	kmod-usb-storage kmod-usb-printer \
 	luci luci-app-diag-devinfo luci-app-p910nd luci-app-qos luci-app-wol \
 	librt libiw query3g maccalc libopenssl \
-	usb-modeswitch usb-modeswitch-data usbutils
+	usb-modeswitch usb-modeswitch-data usbutils \
+	firewall
 endef
 
 define Profile/nprove/Description
