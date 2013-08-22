e3b02a9e3c7ac380b186563eaf25e690490c6a17
Artur Artamonov <freeartman@wechall.net>
2013-08-22 09:52:21 +0300

    [realtek] Fixed profiles

---
+++ target/linux/realtek/rtl8196c/profiles/nprove.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2011 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/NPROVE
+	NAME:=Nprove router
+	PACKAGES:= \
+	kmod-tun kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+	kmod-usb-serial kmod-usb-serial-option \
+	kmod-usb-storage kmod-usb-printer \
+	luci luci-app-diag-devinfo luci-app-p910nd luci-app-qos luci-app-wol \
+	librt libiw query3g maccalc libopenssl \
+	usb-modeswitch usb-modeswitch-data usbutils \
+	firewall
+endef
+
+define Profile/NPROVE/Description
+	Package set optimized for the Nprove boards.
+endef
+
+$(eval $(call Profile,NPROVE))
+
