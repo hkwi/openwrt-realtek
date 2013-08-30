1a85a25384f3860332bb8840c1a603302581424c
Roman Yeryomin <roman@advem.lv>
2013-08-30 11:47:54 +0300

    Add vfat and mountd to nprove profiles.
    Signed-off-by: Roman Yeryomin <roman@advem.lv>

--- target/linux/realtek/rtl8196d/profiles/nprove.mk
+++ target/linux/realtek/rtl8196d/profiles/nprove.mk
@@ -8,13 +8,13 @@
 define Profile/nprove
 	NAME:=nprove
 	PACKAGES:= \
-		kmod-tun kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+		kmod-tun kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 kmod-fs-vfat \
 		kmod-usb-serial kmod-usb-serial-option \
 		kmod-usb-storage kmod-usb-printer \
 		luci luci-app-diag-devinfo luci-app-p910nd luci-app-qos luci-app-wol \
 		librt libiw query3g maccalc libopenssl \
 		usb-modeswitch usb-modeswitch-data usbutils \
-		firewall
+		firewall mountd
 endef
 
 define Profile/nprove/Description
